29dbaf495313701617c0c43330dc77f6e5020b41
Hernán de la Fuente <hernan.de@mercadolibre.com>
2021-12-22 20:49:38 -0300

    avances

--- classifier.rb
+++ classifier.rb
@@ -1,5 +1,27 @@
 require 'csv'
-require_relative './ruby-continent/lib/continent'
+require_relative '../ruby-continent/lib/continent'
+
+TOTAL_ACES = {
+    TOO_FEW: [0, 250],
+    FEW: [250, 500],
+    MEDIUM: [500, 1300],
+    TOO_MUCH: [1300, 2000],
+    KARLOVICH: [2000, 999999999]
+}
+
+TOTAL_DF = {
+    FEW: [0, 200],
+    NORMAL: [200, 400],
+    TOO_MUCH: [400, 700],
+    AMATEUR: [700, 99999]
+}
+
+RANKING_DIFF = {
+    TOO_FEW: [0, 20],
+    FEW: [20, 40],
+    NORMAL: [40, 80],
+    TOO_MUCH: [80, 999999999]
+}
 
 PLAYER_HEIGHTS = {
     Short: [0, 180],
@@ -25,7 +47,7 @@ PLAYER_RANK = {
 
 
 def main
-    rows = CSV.read("./data_copy.csv")
+    rows = CSV.read("./parsed_recortado.csv")
 
     rows[0] << 'winner'
 
@@ -33,36 +55,98 @@ def main
         if index < rows.count / 2
             row << 'A'
         else
-            winner_data = row[0..3]
-            looser_data = row[4..7]
-
-            row[0..3] = looser_data
-            row[4..7] = winner_data
+            winner_data = []
+            loser_data = []
+            winner_data << row[0]
+            winner_data << row[1]
+            loser_data << row[2]
+            loser_data << row[3]
+            winner_data << row[4]
+            winner_data << row[5]
+            loser_data << row[6]
+            loser_data << row[7]
+            winner_data << row[8]
+            loser_data << row[9]
+            
+            row[0] = winner_data[0]
+            row[1] = winner_data[1]
+            row[2] = loser_data[0]
+            row[3] = loser_data[1]
+            row[4] = winner_data[2]
+            row[5] = winner_data[3]
+            row[6] = loser_data[2]
+            row[7] = loser_data[3]
+            row[8] = winner_data[4]
+            row[9] = loser_data[4]
+            
             row << 'B'
         end
 
         row = classify_player_height(row)
         row = classify_player_origin(row)
-        row = classify_player_age(row)
-        row = classify_player_rank(row)
+        row = classify_total_aces(row)
+        row = classify_total_df(row)
+        row = classify_ranking_diff(row)
+        #row = classify_player_age(row)
+        #row = classify_player_rank(row)
     end
 
     CSV.open('./data.csv', 'wb') { |csv| rows.each{|row| csv << row}}
+end
+
+def classify_total_aces(row)
+    TOTAL_ACES.each_key do |category|
+        min, max = TOTAL_ACES[category]
+        row[4] = category if row[4].class != Symbol && row[4].to_i >= min && row[4].to_i < max
+        row[6] = category if row[6].class != Symbol && row[6].to_i >= min && row[6].to_i < max
+    end
+    row
+end
+
+def classify_total_df(row)
+    TOTAL_DF.each_key do |category|
+        min, max = TOTAL_DF[category]
+        row[5] = category if row[5].class != Symbol && row[5].to_i >= min && row[5].to_i < max
+        row[7] = category if row[7].class != Symbol && row[7].to_i >= min && row[7].to_i < max
+    end
+    row
+end
+
+def classify_ranking_diff(row)
+    RANKING_DIFF.each_key do |category|
+        min, max = RANKING_DIFF[category]
+        row[10] = category if row[10].class != Symbol && row[10].to_i >= min && row[10].to_i < max
+    end
+    row
 end
 
 def classify_player_height(row)
     PLAYER_HEIGHTS.each_key do |category|
         min, max = PLAYER_HEIGHTS[category]
         row[0] = category if row[0].class != Symbol && row[0].to_i >= min && row[0].to_i < max
-        row[4] = category if row[4].class != Symbol && row[4].to_i >= min && row[4].to_i < max
+        row[2] = category if row[2].class != Symbol && row[2].to_i >= min && row[2].to_i < max
     end
 
     row
 end
 
 def classify_player_origin(row)
-    row[1] = Continent.by_alpha_3_code(row[1]).nil? ? 'other' : Continent.by_alpha_3_code(row[1])[:continent_codes][0]
-    row[5] = Continent.by_alpha_3_code(row[5]).nil? ? 'other' : Continent.by_alpha_3_code(row[5])[:continent_codes][0]
+    # por valid counter
+    valid_country = ['ESP', 'FRA', 'GER', 'USA', 'RUS', 'CHN', 'ARG', 'BRA', 'SVK', 'GBR', 'ITA', 'SUI', 'CRO', 'SRB', 'CZE']
+     if (row[1].nil? || !(valid_country.include? row[1]))
+         row[1] = 'other'
+     end
+     if (row[3].nil? || !(valid_country.include? row[3]))
+         row[3] = 'other'
+     end
+
+    # por continente
+    #row[1] = Continent.by_alpha_3_code(row[1]).nil? ? 'other' : Continent.by_alpha_3_code(row[1])[:continent_codes][0]
+    #row[3] = Continent.by_alpha_3_code(row[3]).nil? ? 'other' : Continent.by_alpha_3_code(row[3])[:continent_codes][0]
+
+    # por EU  / otro
+    #row[1] = Continent.by_alpha_3_code(row[1]).nil? || Continent.by_alpha_3_code(row[1])[:continent_codes][0] != 'EU' ? 'other' : Continent.by_alpha_3_code(row[1])[:continent_codes][0]
+    #row[3] = Continent.by_alpha_3_code(row[3]).nil? || Continent.by_alpha_3_code(row[3])[:continent_codes][0] != 'EU' ? 'other' : Continent.by_alpha_3_code(row[3])[:continent_codes][0]
 
     row
 end
--- initial_parsing.rb
+++ initial_parsing.rb
@@ -14,23 +14,78 @@ UNWANTED_COLUMNS_INDEXES_BY_NAME = {
   match_num: 6,
   winner_seed: 8,
   winner_entry: 9,
-  loser_seed: 19,
-  loser_entry: 20,
-  best_of: 29,
-  round: 30,
+  loser_seed: 18,
+  loser_entry: 19,
+  best_of: 28,
+  round: 29,
 }
+
+WINER_ID_COLUMN = 7
+WINNER_AGE_COLUMN = 14
+WINER_RANK_COLUMN = 15
+WINER_ACES_COLUMN = 31
+WINER_DF_COLUMN = 32
+LOSER_ID_COLUMN = 17
+LOSER_AGE_COLUMN = 24
+LOSER_RANK_COLUMN = 25
+LOSER_ACES_COLUMN = 40
+LOSER_DF_COLUMN = 41
+
+class PlayerData
+
+  attr_accessor :id, :total_aces, :total_df, :matches
+
+  def initialize(id)
+    @id = id
+    @total_aces = 0
+    @total_df = 0
+    @matches = {}
+  end
+
+  def add_winner_data(row)
+    @total_aces += row[WINER_ACES_COLUMN].to_i
+    @total_df += row[WINER_DF_COLUMN].to_i
+    unless matches[row[LOSER_ID_COLUMN]].nil?
+      matches[row[LOSER_ID_COLUMN]] += 1
+    else
+      matches[row[LOSER_ID_COLUMN]] = 1
+    end
+  end
+
+  def add_loser_data(row)
+    @total_aces += row[LOSER_ACES_COLUMN].to_i
+    @total_df += row[LOSER_DF_COLUMN].to_i
+    unless matches[row[WINER_ID_COLUMN]].nil?
+      matches[row[WINER_ID_COLUMN]] -= 1
+    else
+      matches[row[WINER_ID_COLUMN]] = -1
+    end
+  end
+
+end
 
 def main
   headers = CSV.read("./atp_matches_2000.csv")[0]
   headers = headers.reject{ |column| UNWANTED_COLUMNS_INDEXES_BY_NAME.keys.include?(column.to_sym) }
   headers.insert(1, 'year')
+  headers << 'winner_total_aces'
+  headers << 'winner_total_df'
+  headers << 'loser_total_aces'
+  headers << 'loser_total_df'
+  headers << 'winner_is_head'
+  headers << 'loser_is_head'
+  headers << 'ranking_diff'
+  headers << 'age_diff'
 
   CSV.open('./parsed.csv', 'wb', headers: headers, write_headers: true) do |csv|
+
+    player_data_list = {}
 
     (2000..2017).each do |year|
       rows = CSV.read("./atp_matches_#{year}.csv")
       rows.shift
       rows = rows.map do |row|
+          row = add_complementary_data(player_data_list, row)
           row = remove_unwanted_columns(row)
           row = add_year_column(row)
           row
@@ -38,6 +93,34 @@ def main
       rows.each{|row| csv << row}
     end
   end
+end
+
+def add_complementary_data(player_data_list, row)
+  winner_id = row[WINER_ID_COLUMN]
+  loser_id = row[LOSER_ID_COLUMN]
+
+  player_data_list[winner_id] ||= PlayerData.new(winner_id)
+  player_data_list[loser_id] ||= PlayerData.new(loser_id)
+
+  winner_is_head = (player_data_list[winner_id].matches[loser_id] || 0) > 0
+  loser_is_head = (player_data_list[loser_id].matches[winner_id] || 0) > 0
+  ranking_diff = (row[WINER_RANK_COLUMN].to_i - row[LOSER_RANK_COLUMN].to_i).abs()
+  age_diff = (row[WINNER_AGE_COLUMN].to_i - row[LOSER_AGE_COLUMN].to_i).abs()
+
+  player_data_list[winner_id].add_winner_data(row)
+  player_data_list[loser_id].add_loser_data(row)
+
+  row << player_data_list[winner_id].total_aces
+  row << player_data_list[winner_id].total_df
+
+  row << player_data_list[loser_id].total_aces
+  row << player_data_list[loser_id].total_df
+
+  row << winner_is_head
+  row << loser_is_head
+  row << ranking_diff
+  row << age_diff
+  row
 end
 
 def add_year_column(row)
